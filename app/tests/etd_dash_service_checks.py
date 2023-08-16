14ede22cecf3866d6d6bd6b520a73ebd0ba13f82
Michael Vandermillen <michael_vandermillen@harvard.edu>
2023-08-16 17:05:45 -0400

    only push/pull dash test submission if dash integration feature flag off

--- app/tests/etd_dash_service_checks.py
+++ app/tests/etd_dash_service_checks.py
@@ -24,36 +24,56 @@ class ETDDashServiceChecks():
         incomingDir = "incoming/gsd"
         zipFile = "submission_999999.zip"
 
+        FEATURE_FLAGS = "feature_flags"
+        DASH_FEATURE_FLAG = "dash_feature_flag"
+
         # Send a simple task (create and send in 1 step)
         # res = client.send_task('tasks.tasks.do_task',
         # args=[{"job_ticket_id":"123","hello":"world"}],
         # kwargs={}, queue=incoming_queue)
         # read from 'final_queue' to see that it went through the pipeline
+
+        client = Celery('app')
+        client.config_from_object('celeryconfig')
+
         messagefile = os.environ.get('MESSAGE_FILE', "message.json")
         with open(messagefile) as f:
             messagejson = f.read()
         message = json.loads(messagejson)
-        client = Celery('app')
-        client.config_from_object('celeryconfig')
 
-        try:
-            # cnopts = pysftp.CnOpts()
-            # cnopts.hostkeys = known_hosts
-            # cnopts = pysftp.CnOpts()
-            # cnopts.hostkeys = None
-            with pysftp.Connection(host=remoteSite,
-                                   username=remoteUser,
-                                   private_key=private_key) as sftp:
-                if sftp.exists(f"{archiveDir}/{zipFile}"):
-                    sftp.remove(f"{archiveDir}/{zipFile}")
-                sftp.put(f"./testdata/{zipFile}",
-                         f"{incomingDir}/{zipFile}")
-        except Exception as err:
-            result["num_failed"] += 1
-            result["tests_failed"].append("SFTP")
-            result["info"] = {"Proquest Dropbox sftp failed":
-                              {"status_code": 500,
-                               "text": str(err)}}
+        # only prep test object if dash integration feature flag is on
+        if FEATURE_FLAGS in message:
+            feature_flags = message[FEATURE_FLAGS]
+            # new_message[FEATURE_FLAGS] = feature_flags
+            if (DASH_FEATURE_FLAG in feature_flags and
+                    feature_flags[DASH_FEATURE_FLAG] == "on"):
+
+                # clear out any old test object
+                if glob.glob('/home/etdadm/data/in/proquest*-999999-gsd/submission_999999.zip'):  # noqa: E501
+                    for filename in glob.glob('/home/etdadm/data/in/proquest*-999999-gsd/*'):  # noqa: E501
+                        os.remove(filename)
+                    for filename in glob.glob('/home/etdadm/data/in/proquest*-999999-gsd'):  # noqa: E501
+                        shutil.rmtree(filename)
+
+                # put the test object in the dropbox
+                try:
+                    # cnopts = pysftp.CnOpts()
+                    # cnopts.hostkeys = known_hosts
+                    # cnopts = pysftp.CnOpts()
+                    # cnopts.hostkeys = None
+                    with pysftp.Connection(host=remoteSite,
+                                           username=remoteUser,
+                                           private_key=private_key) as sftp:
+                        if sftp.exists(f"{archiveDir}/{zipFile}"):
+                            sftp.remove(f"{archiveDir}/{zipFile}")
+                        sftp.put(f"./testdata/{zipFile}",
+                                 f"{incomingDir}/{zipFile}")
+                except Exception as err:
+                    result["num_failed"] += 1
+                    result["tests_failed"].append("SFTP")
+                    result["info"] = {"Proquest Dropbox sftp failed":
+                                      {"status_code": 500,
+                                       "text": str(err)}}
 
         client.send_task(name="etd-dash-service.tasks.send_to_dash",
                          args=[message], kwargs={}, queue=incoming_queue)
@@ -61,17 +81,5 @@ class ETDDashServiceChecks():
         sleep_secs = int(os.environ.get('SLEEP_SECS', 2))
 
         time.sleep(sleep_secs)  # wait for queue
-
-        if not glob.glob('/home/etdadm/data/in/proquest*-999999-gsd/submission_999999.zip'):  # noqa: E501
-            result["num_failed"] += 1
-            result["tests_failed"].append("DATA IN")
-            result["info"] = result["info"] | \
-                {"Failed file check": {"status_code": 500,
-                                       "text": "No submission found"}}
-        else:
-            for filename in glob.glob('/home/etdadm/data/in/proquest*-999999-gsd/*'):  # noqa: E501
-                os.remove(filename)
-            for filename in glob.glob('/home/etdadm/data/in/proquest*-999999-gsd'):  # noqa: E501
-                shutil.rmtree(filename)
 
         return result
