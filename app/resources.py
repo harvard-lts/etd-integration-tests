6ce32742027506b3ce3a73f857a4beefefc5798c
Dee Dee Crema <valdeva_crema@harvard.edu>
2023-11-02 23:39:50 -0400

    audio

--- app/resources.py
+++ app/resources.py
@@ -132,3 +132,17 @@ def define_resources(app):
         result["info"] = result["info"] | end_to_end_result["info"]
 
         return json.dumps(result)
+    
+    @app.route('/etd_with_audio_test')
+    def etd_with_audio_test():
+        result = {"num_failed": 0,
+                  "tests_failed": [], "info": {}}
+
+        dais_end_to_end_image = ETDDAISEndToEnd()
+        end_to_end_result = dais_end_to_end_image.end_to_end_images_test()
+        result["num_failed"] = end_to_end_result["num_failed"]
+        if len(end_to_end_result["tests_failed"]) > 0:
+            result["tests_failed"].append(end_to_end_result["tests_failed"])
+        result["info"] = result["info"] | end_to_end_result["info"]
+
+        return json.dumps(result)
--- app/tests/etd_dais_end_to_end.py
+++ app/tests/etd_dais_end_to_end.py
@@ -125,15 +125,15 @@ class ETDDAISEndToEnd():
                                "text": str(e)}}
 
         return result
-    
+
     def end_to_end_opaque_gif_test(self):
         """
         This function performs an end-to-end test for the ETD system using
-        image files. It copies a test submission file from the data directory
-        to the ETD 'in' directory, builds DRS Admin MD for the opaque and
-        images, and calls DIMS. It returns a dictionary with the number of
-        failed tests, a list of failed tests, and additional information
-        about the failed tests if any.
+        opaque and image files. It copies a test submission file from the
+        data directory to the ETD 'in' directory, builds DRS Admin MD for
+        the opaque and images, and calls DIMS. It returns a dictionary with
+        the number of failed tests, a list of failed tests, and additional
+        information about the failed tests if any.
 
         Returns:
             dict: A dictionary with the following keys:
@@ -164,6 +164,64 @@ class ETDDAISEndToEnd():
         if dest_path:
             payload_data = self. \
                 __build_drs_admin_md_for_opaque_gif(dest_path)
+        else:
+            result["num_failed"] += 1
+            result["tests_failed"].append("Copy failed")
+            result["info"] = {"Copy failed":
+                              {"status_code": 500,
+                               "text": "Copy failed"}}
+            return result
+
+        try:
+            # Call DIMS
+            self.__call_dims(payload_data)
+        except Exception as e:
+            result["num_failed"] += 1
+            result["tests_failed"].append("DIMS call failed")
+            result["info"] = {"DIMS Call failed":
+                              {"status_code": 500,
+                               "text": str(e)}}
+
+        return result
+
+    def end_to_end_audio_test(self):
+        """
+        This function performs an end-to-end test for the ETD system using
+        audio files. It copies a test submission file from the data directory
+        to the ETD 'in' directory, builds DRS Admin MD for the audio,
+        and calls DIMS. It returns a dictionary with the number of
+        failed tests, a list of failed tests, and additional information
+        about the failed tests if any.
+
+        Returns:
+            dict: A dictionary with the following keys:
+                - num_failed (int): The number of failed tests.
+                - tests_failed (list): A list of failed tests.
+                - info (dict): Additional information about the failed tests
+                if any.
+        """
+        result = {"num_failed": 0,
+                  "tests_failed": [],
+                  "info": {}}
+
+        try:
+            # Copy test submission file from data dir to ETD 'in' directory
+            zip_file = "submission_audio.zip"
+            dest_path = self.__copy_test_submission(
+                zip_file, "end_to_end_audio_test")
+        except Exception as e:
+            result["num_failed"] += 1
+            result["tests_failed"].append("Copy failed with exception")
+            result["info"] = {"Copy failed with exception":
+                              {"status_code": 500,
+                               "text": str(e)}}
+            return result
+
+        payload_data = {}
+        # Build DRS Admin MD
+        if dest_path:
+            payload_data = self. \
+                __build_drs_admin_md_for_audio(dest_path)
         else:
             result["num_failed"] += 1
             result["tests_failed"].append("Copy failed")
@@ -265,7 +323,8 @@ class ETDDAISEndToEnd():
 
         thesis_name = "Alfred_S_MArchI_F21 Thesis.pdf"
         license_name = "setup_2E592954-F85C-11EA-ABB1-E61AE629DA94.pdf"
-        osn_unique_appender = "ETD_THESIS_gsd_2022-05_PQ_28963877_" + timestamp_appender
+        osn_unique_appender = "ETD_THESIS_gsd_2022-05_PQ_28963877_"
+        + timestamp_appender
         file_info = {
             thesis_name: {
                 "modified_file_name":
@@ -340,7 +399,7 @@ class ETDDAISEndToEnd():
                         }
                         }
         return payload_data
-    
+
     def __build_drs_admin_md_for_images(self, dest_path):
         # Create a unique OSN based on the timestamp
         timestamp_appender = str(int(datetime.now().timestamp()))
@@ -424,6 +483,108 @@ class ETDDAISEndToEnd():
                             "mmsid": "12345",
                             "dash_id": "TEST1234",
                             "pq_id": "PQ-28542548",
+                            "alma_id": "99156631569803941",
+                            "file_info": file_info
+                        }
+                        }
+        return payload_data
+
+    def __build_drs_admin_md_for_audio(self, dest_path):
+        # Create a unique OSN based on the timestamp
+        timestamp_appender = str(int(datetime.now().timestamp()))
+
+        thesis_name = "MLA Thesis_Auger_Catherine_May2023.pdf"
+        license_name = "setup_2E592954-F85C-11EA-ABB1-E61AE629DA94.pdf"
+        osn_unique_appender = "ETD_THESIS_gsd_2023-05_PQ_30494273_" + timestamp_appender
+        file_info = {
+            thesis_name: {
+                "modified_file_name":
+                "MLA_Thesis_Auger_Catherine_May2023.pdf",
+                "file_role": "ARCHIVAL_MASTER",
+                "object_role": "THESIS",
+                "object_osn": "ETD_THESIS_" + osn_unique_appender,
+                "file_osn": "ETD_THESIS_" + osn_unique_appender + "_1"
+            },
+            license_name: {
+                "modified_file_name":
+                "setup_2E592954-F85C-11EA-ABB1-E61AE629DA94.pdf",
+                "file_role": "LICENSE",
+                "object_role": "LICENSE",
+                "object_osn": "ETD_LICENSE_" + osn_unique_appender,
+                "file_osn": "ETD_LICENSE_" + osn_unique_appender + "_1"
+            },
+            "mets.xml": {
+                "modified_file_name": "mets.xml",
+                "file_role": "DOCUMENTATION",
+                "object_role": "DOCUMENTATION",
+                "object_osn": "ETD_DOCUMENTATION_" + osn_unique_appender,
+                "file_osn": "ETD_DOCUMENTATION_" + osn_unique_appender + "_1"
+            },
+            "Gamelan_Islam.mp3": {
+                "modified_file_name": "Gamelan_Islam.mp3",
+                "file_role": "ARCHIVAL_MASTER",
+                "object_role": "THESIS_SUPPLEMENT",
+                "object_osn": "ETD_SUPPLEMENT_" + osn_unique_appender + "_1",
+                "file_osn": "ETD_SUPPLEMENT__" + osn_unique_appender + "_1_1"
+            },
+            "Harry Styles.mp3": {
+                "modified_file_name": "Harry_Styles.mp3",
+                "file_role": "ARCHIVAL_MASTER",
+                "object_role": "THESIS_SUPPLEMENT",
+                "object_osn": "ETD_SUPPLEMENT__" + osn_unique_appender + "_2",
+                "file_osn": "ETD_SUPPLEMENT__" + osn_unique_appender + "_2_1"
+            },
+            "Jalan Raya Ubud.mp3": {
+                "modified_file_name": "Jalan_Raya_Ubud.mp3",
+                "file_role": "ARCHIVAL_MASTER",
+                "object_role": "THESIS_SUPPLEMENT",
+                "object_osn": "ETD_SUPPLEMENT__" + osn_unique_appender + "_3",
+                "file_osn": "ETD_SUPPLEMENT__" + osn_unique_appender + "_3_1"
+            },
+            "Kuningan.mp3": {
+                "modified_file_name": "Kuningan.mp3",
+                "file_role": "ARCHIVAL_MASTER",
+                "object_role": "THESIS_SUPPLEMENT",
+                "object_osn": "ETD_SUPPLEMENT__" + osn_unique_appender + "_4",
+                "file_osn": "ETD_SUPPLEMENT__" + osn_unique_appender + "_4_1"
+            },
+            "Monkey Forest.mp3": {
+                "modified_file_name": "Monkey_Forest.mp3",
+                "file_role": "ARCHIVAL_MASTER",
+                "object_role": "THESIS_SUPPLEMENT",
+                "object_osn": "ETD_SUPPLEMENT__" + osn_unique_appender + "_5",
+                "file_osn": "ETD_SUPPLEMENT__" + osn_unique_appender + "_5_1"
+            },
+            "Pasar Goris.mp3": {
+                "modified_file_name": "Pasar_Goris.mp3",
+                "file_role": "ARCHIVAL_MASTER",
+                "object_role": "THESIS_SUPPLEMENT",
+                "object_osn": "ETD_SUPPLEMENT__" + osn_unique_appender + "_6",
+                "file_osn": "ETD_SUPPLEMENT__" + osn_unique_appender + "_6_1"
+            },
+            "The Pipe.mp3": {
+                "modified_file_name": "The_Pipe.mp3",
+                "file_role": "ARCHIVAL_MASTER",
+                "object_role": "THESIS_SUPPLEMENT",
+                "object_osn": "ETD_SUPPLEMENT__" + osn_unique_appender + "_7",
+                "file_osn": "ETD_SUPPLEMENT__" + osn_unique_appender + "_7_1"
+            }
+        }
+        payload_data = {"package_id": "ETD_TESTING_" + timestamp_appender,
+                        "fs_source_path": dest_path,
+                        "s3_path": "",
+                        "s3_bucket_name": "",
+                        "depositing_application": "ETD",
+                        "admin_metadata": {
+                            "depositingSystem": "ETD",
+                            "ownerCode": "HUL.TEST",
+                            "billingCode": "HUL.TEST.BILL_0001",
+                            "urnAuthorityPath": "HUL.TEST",
+                            "original_queue": "test",
+                            'task_name': "test",
+                            "retry_count": 0,
+                            "dash_id": "TEST1234",
+                            "pq_id": "PQ-30494273",
                             "alma_id": "99156631569803941",
                             "file_info": file_info
                         }
