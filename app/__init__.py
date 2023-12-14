44c204c54c9f869f8b70b9084f3829ea222ee778
Chip Goines <chip_goines@harvard.edu>
2023-12-14 16:31:20 -0500

    ETD-281: productionize logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,18 @@
 import os
-import click
 import logging
-import traceback
+import socket
+from datetime import datetime
 from logging.handlers import TimedRotatingFileHandler
-from flask import Flask, current_app
+from flask import Flask
 # Import custom modules from the local project
 # Import API resources
 from . import resources
 
 LOG_FILE_BACKUP_COUNT = 1
 LOG_ROTATION = "midnight"
+
+container_id = socket.gethostname()
+timestamp = datetime.today().strftime('%Y-%m-%d')
 
 
 # App factory
@@ -31,18 +34,24 @@ def create_app():
 
 def configure_logger():
     log_level = os.getenv("APP_LOG_LEVEL", "INFO")
-    log_dir = os.getenv("LOG_DIR", "/home/etdadm/logs/etd_itest")
-    log_file_path = os.path.join(log_dir, "int_tests.log")
+    log_file_path = os.getenv("LOG_DIR", "/home/etdadm/logs/etd_itest")
     formatter = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+                '%(asctime)s - %(name)s - %(levelname)s - ' +
+                '[%(filename)s:%(funcName)s:%(lineno)d] - %(message)s')
 
-    file_handler = TimedRotatingFileHandler(
-        filename=log_file_path,
-        when=LOG_ROTATION,
-        backupCount=LOG_FILE_BACKUP_COUNT
-    )
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
 
     logger = logging.getLogger('etd_int_tests')
-    logger.addHandler(file_handler)
-    file_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    # Defaults to console logging
+    if os.getenv("CONSOLE_LOGGING_ONLY", "true") == "false":
+        file_handler = TimedRotatingFileHandler(
+            filename=f"{log_file_path}/{container_id}_console_{timestamp}.log",
+            when=LOG_ROTATION,
+            backupCount=LOG_FILE_BACKUP_COUNT
+        )
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
     logger.setLevel(log_level)
